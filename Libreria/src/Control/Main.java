cb355552d9802db4537f5e3b0da9f62d117c0a2f
castanos20 <castanojose158@gmail.com>
2025-01-28 19:19:59 -0500

    nuevaprueba

--- Libreria/src/Control/Main.java
+++ Libreria/src/Control/Main.java
@@ -6,6 +6,12 @@ public class Main {
 
 	public static void main(String []args) {
 		InOut in=new InOut();
-		in.mostrartext("pan");
+		in.mostrartext("Ingrese 1 o 2");
+		int a = in.pedirint(null);
+		if(a==1) {
+			in.mostrartext("Hello wolrd");
+		}
+		else 
+			in.mostrartext("Hola soy jose");
 	}
 }
--- Libreria/src/Vista/InOut.java
+++ Libreria/src/Vista/InOut.java
@@ -2,9 +2,16 @@ package Vista;
 import java.util.Scanner;
 
 public class InOut {
-
+	Scanner sc=new Scanner(System.in);
+	
 	public void mostrartext(String texto) {
 		System.out.print(texto);
+	}
+	
+	public int pedirint(String mensaje) {
+		mostrartext(mensaje);
+		int dato = sc.nextInt();
+		return dato;
 	}
 	
 }
